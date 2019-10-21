a4d53e0cf52968d3b43c8bd65ea6f1c6c241a378
jeferson <jeferson.silva@ccc.ufcg.edu.br>
2019-10-21 17:05:12 -0300

    add function to get an image histogram

--- analysis/extracting_features.py
+++ analysis/extracting_features.py
@@ -4,6 +4,14 @@ import os
 
 SAMPLE_FOLDER = "../sample_images" # if you can't find images, try run this file in analysis folder
 
+def get_histogram(image, bins=256):
+    r''' returns a dict with histogram per channel of an image '''
+    color = ('b','g','r')
+    hist_dict = {'r': [], 'g': [], 'b': []}
+    for channel, col in enumerate(color):
+        hist = cv2.calcHist([img],[channel],None,[bins],[0,bins])
+        hist_dict[color].extend(hist)
+    return hist_dict
 
 # get the file names from a folder
 def read_video_names(video_folder):
@@ -18,6 +26,8 @@ def read_video_names(video_folder):
 img_folder = read_video_names(SAMPLE_FOLDER)
 
 for img_name in img_folder:
-    img=cv2.imread(SAMPLE_FOLDER + "/" +img_name)
+    img = cv2.imread(SAMPLE_FOLDER + "/" +img_name)
+    hist = get_histogram(img)
     cv2.imshow('i', img)
     cv2.waitKey(0)
+    break
