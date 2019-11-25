d36f4976e144b7eb029f5427c6945c638d715901
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-11-24 23:18:04 -0300

    spliting data between test and train

--- analysis/extracting_features.py
+++ analysis/extracting_features.py
@@ -1,7 +1,17 @@
 import cv2
 import os
+from sklearn.model_selection import train_test_split
+import shutil
+import mahotas
 
 SAMPLE_FOLDER = "../sample_images" # if you can't find images, try run this file in analysis folder
+TRAIN_NAMES="trainImages.csv"
+TEST_NAMES="testImages.csv"
+IMG_FOLDER="../img"
+TEST_FEATURES_FILE="testFeatures.csv"
+TRAIN_FEATURES_FILE="trainFeatures.csv"
+TEST_FOLDER="test/"
+TRAIN_FOLDER="train/"
 
 def get_histogram(image, bins=256):
     r''' returns a dict with histogram per channel of an image '''
@@ -12,21 +22,87 @@ def get_histogram(image, bins=256):
         hist_dict[col].extend(hist.reshape(-1))
     return hist_dict
 
+def fd_hu_moments(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    feature = cv2.HuMoments(cv2.moments(image)).flatten()
+    return feature
+
+def fd_haralick(image):    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # compute the haralick texture feature vector
+    haralick = mahotas.features.haralick(gray).mean(axis=0)
+    return haralick
+
 # get the file names from a folder
-def read_video_names(video_folder):
+def create_train_test(folder_name):
     video_filename = []
-    for (dirpath, dirnames, filename) in os.walk(video_folder):
 
-        video_filename.extend(filename)
+    # create these files before run
+    f = open(TRAIN_NAMES, "a")
+    f_test = open(TEST_NAMES, "a")
+    for (dirpath, dirnames, filename) in os.walk(folder_name):
 
-    return(video_filename)
+        label = dirpath.split("/")[-1]
+        if label != folder_name:
+            label = [label]*len(filename)
+            Xi_train, Xi_test, yi_train, yi_test = train_test_split(filename, label, test_size=0.30, random_state=42)
+            
+            for i in range(len(Xi_train)):
+                f.write(Xi_train[i] + ";" + yi_train[i]+"\n")
 
+            for i in range(len(Xi_test)):
+                f_test.write(Xi_test[i] + ";" + yi_test[i]+"\n")
+    f.close()
+    f_test.close()
 
-img_folder = read_video_names(SAMPLE_FOLDER)
+def split_data():
+    test_path = IMG_FOLDER + "/" + TEST_FOLDER
+    train_path = IMG_FOLDER + "/" + TRAIN_FOLDER
+    test_feactures = open(TEST_FEATURES_FILE, "a")
+    train_feactures = open(TRAIN_FEATURES_FILE, "a")
+    os.mkdir(test_path)
+    os.mkdir(train_path)
 
-for img_name in img_folder:
-    img = cv2.imread(SAMPLE_FOLDER + "/" +img_name)
+    with open(TRAIN_NAMES, "r") as f:
+        line = f.readline()
+        while(line):
+            img_name, folder_name = line.split(";")
+            folder_name = folder_name[:-1] # remove '\n'
+            img_path = IMG_FOLDER + "/" + folder_name + "/" + img_name
+            train_feactures.write(extract_feature(img_path)+";"+folder_name+"\n")
+            destination = train_path + folder_name
+            if not os.path.exists(destination):
+                os.mkdir(destination)
+                
+            shutil.copy(img_path, destination)
+            line = f.readline()
+    f.close()    
+
+    with open(TEST_NAMES, "r") as f:
+        line = f.readline()
+        while(line):
+            img_name, folder_name = line.split(";")
+            folder_name = folder_name[:-1]
+            img_path = IMG_FOLDER + "/" + folder_name + "/" + img_name
+            test_feactures.write(extract_feature(img_path)+";"+folder_name+"\n")
+            destination = test_path + folder_name
+            if not os.path.exists(destination):
+                os.mkdir(destination)
+                
+            shutil.copy(img_path, destination)
+            line = f.readline()
+    f.close()    
+
+def extract_feature(img_name):
+    img = cv2.imread(img_name)
     hist = get_histogram(img)
-    cv2.imshow('i', img)
-    cv2.waitKey(0)
-    break
+    moment = fd_hu_moments(img)
+    hara = fd_haralick(img)
+
+    return str(hist)+";"+ str(moment)+";"+ str(hara)
+
+# separate IMG_FOLDER images between train and test .csv
+create_train_test(IMG_FOLDER)
+
+# extract features and create folders with test and train images
+split_data()
